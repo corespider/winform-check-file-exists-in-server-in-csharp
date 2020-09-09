3bd9114f3aa4965c17f8e9dd0d1bf88e94ee2d3c
Your Name <corespiderpro@gmail.com>
2020-09-09 15:58:58 +0530

    Add project files.

---
+++ WinForm-CheckFileServer/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WinForm_CheckFileServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(342, 227);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(148, 45);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check in Server";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(252, 164);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(426, 26);
+            this.txtURL.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Check if file exists in Server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WinForm-CheckFileServer/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_CheckFileServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            bool result = CheckFile(txtURL.Text);
+            if (result)
+            {
+                MessageBox.Show("The File is exists on WWW", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("The File is not found on the server", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private bool CheckFile(string URL)
+        {
+            try
+            {
+
+                HttpWebRequest request = WebRequest.Create(URL) as HttpWebRequest;
+                request.Method = "HEAD";
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    return (response.StatusCode == HttpStatusCode.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+    }
+}
